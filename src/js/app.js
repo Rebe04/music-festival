d3ec8be98aed1425908364074ba5ed89d899e341
Rebe04 <e.benitez@estebanbenitez.com>
2025-01-04 21:54:59 +1030

    Feat: Gallery section has been created using JS and SCSS

---
+++ src/js/app.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+
+function crearGaleria() {
+  const cantidadImagenes = 16;
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= cantidadImagenes; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = `imagen Galeria ${i}`;
+
+    //Event Handler
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+    galeria.appendChild(imagen);
+  }
+}
+
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = `imagen Galeria ${i}`;
+
+  //generar Modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+
+  //Boton Cerrar Modal
+  const cerrarModalBtn = document.createElement("BUTTON");
+  cerrarModalBtn.textContent = "X";
+  cerrarModalBtn.classList.add("btn-cerrar");
+  cerrarModalBtn.onclick = cerrarModal;
+
+  modal.appendChild(imagen);
+  modal.appendChild(cerrarModalBtn);
+
+  //Agregar al HTML;
+
+  const body = document.querySelector("body");
+  body.classList.add("overflow-hidden");
+  body.appendChild(modal);
+}
+
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  //   modal.classList.add("fade-out");
+  modal.remove();
+  const body = document.querySelector("body");
+  body.classList.remove("overflow-hidden");
+  //   modal.addEventListener(
+  //     "animationend",
+  //     function () {
+  //       modal.remove();
+  //     },
+  //     { once: true }
+  //   );
+}
