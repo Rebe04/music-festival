f3aa00ef2935229ea286bc62354ec7c7aed764d3
Rebe04 <e.benitez@estebanbenitez.com>
2025-01-04 22:28:56 +1030

    Feat: Links hover according to the section that is being showed has been added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   crearGaleria();
   navegacionFija();
+  resaltarEnlace();
 });
 
 function navegacionFija() {
@@ -72,4 +73,29 @@ function cerrarModal() {
   //     },
   //     { once: true }
   //   );
+}
+
+function resaltarEnlace() {
+  document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+    let actual = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        actual = section.id;
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${actual}`) {
+        link.classList.add("active");
+      }
+    });
+  });
 }
